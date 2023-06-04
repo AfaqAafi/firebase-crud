f91b27eb5c2ddd9fb2d01f311b205efd7830fb64
Muhammad Afaq <dmdcode16@gmail.com>
2023-06-04 21:26:24 +0500

    add delete update fetch

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import {initializeApp} from 'firebase/app'
+import {collection, getFirestore} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAux0NYNoMXWmLoeFErchh0aK5QLGowP80",
+  authDomain: "todoapp-6991d.firebaseapp.com",
+  projectId: "todoapp-6991d",
+  storageBucket: "todoapp-6991d.appspot.com",
+  messagingSenderId: "555190807210",
+  appId: "1:555190807210:web:748867546f8a157b7c7ed3",
+};
+//! init firebase
+const app = initializeApp(firebaseConfig)
+
+// get serviecs
+export const db = getFirestore(app);
+
+export const colRef = collection(db, 'todo');
+
